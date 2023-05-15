4a2b0eedac1309b2671aa22808cab10bffbcb27f
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-05-15 21:03:30 +0200

    cs_physics.py

--- ssls.py
+++ ssls.py
@@ -22,6 +22,7 @@ import time
 
 from cs_animation import CurveSimAnimation
 from cs_parameters import CurveSimParameters, Standard_sections
+from cs_physics import CurveSimPhysics
 
 
 # If you run this script in a jupyter notebook, uncomment this line in order to provide the name of your config file.
@@ -180,7 +181,7 @@ class CurveSimBody:
 
     def calc_state_vectors(self, bodies):
         """Get initial position and velocity of the physical body self."""
-        self.mu = CurveSimPhysics.gravitational_parameter(bodies)  # is the same for all bodies in the system, because they are orbiting a common barycenter
+        self.mu = CurveSimPhysics.gravitational_parameter(bodies, P.g)  # is the same for all bodies in the system, because they are orbiting a common barycenter
         if self.velocity is None:  # State vectors are not in config file. So they will be calculated from Kepler orbit parameters instead.
             pos, vel, *_ = self.keplerian_elements_to_state_vectors()
             self.positions[0] = np.array(pos, dtype=float)  # [m] initial position
@@ -223,63 +224,6 @@ class CurveSimBody:
                 return 0.0, 0.0
         else:  # body cannot eclipse self, because self is nearer to viewer than body
             return 0.0, 0.0
-
-
-class CurveSimPhysics:
-
-    @staticmethod
-    def kepler_equation(ea, e, ma):
-        """ea: eccentric anomaly [rad], e: eccentricity, ma: mean anomaly [rad]"""
-        if not -2 * math.pi < ea < 2 * math.pi:
-            raise ValueError("eccentric anomaly ea must be in radians but is outside of the range ]-2π;2π[")
-        if not -2 * math.pi < ma < 2 * math.pi:
-            raise ValueError("mean anomaly ma must be in radians but is outside of the range ]-2π;2π[")
-        if not 0 <= e < 1:
-            raise ValueError("eccentricity e is outside of the range [0;1[")
-        return ea - e * math.sin(ea) - ma
-
-    @staticmethod
-    def kepler_equation_derivative(ea, e):
-        """ea: eccentric anomaly [rad], e: eccentricity"""
-        return 1.0 - e * math.cos(ea)
-
-    @staticmethod
-    def kepler_equation_root(e, ma, ea_guess=0.0, tolerance=1e-10, max_steps=50):
-        """Calculate the root of the Kepler Equation with the Newton–Raphson method.
-            e: eccentricity, ma: mean anomaly [rad], ea_guess: eccentric anomaly [rad]. ea_guess=ma is a good start."""
-        for n in range(max_steps):
-            delta = CurveSimPhysics.kepler_equation(ea_guess, e, ma) / CurveSimPhysics.kepler_equation_derivative(ea_guess, e)
-            if abs(delta) < tolerance:
-                return ea_guess - delta
-            ea_guess -= delta
-        raise RuntimeError('Newton\'s root solver did not converge.')
-
-    @staticmethod
-    def gravitational_parameter(bodies):
-        """Calculate the gravitational parameter of masses orbiting a common barycenter
-        https://en.wikipedia.org/wiki/Standard_gravitational_parameter"""
-        mass = 0.0
-        for body in bodies:
-            mass += body.mass
-        return P.g * mass
-
-    @staticmethod
-    def distance_2d_ecl(body1, body2, i):
-        """Return distance of the centers of 2 physical bodies as seen by a viewer (projection y->0)."""
-        dx = body1.positions[i][0] - body2.positions[i][0]
-        dz = body1.positions[i][2] - body2.positions[i][2]
-        return math.sqrt((dx ** 2 + dz ** 2))
-
-    @staticmethod
-    def limbdarkening(relative_radius, beta):
-        """https://en.wikipedia.org/wiki/Limb_darkening
-        https://de.wikipedia.org/wiki/Photosph%C3%A4re#Mitte-Rand-Verdunkelung
-        Approximates the flux of a star at a point on the star seen from a very large distance.
-        The point's apparent distance from the star's center is relative_radius * radius.
-        Beta depends on the wavelength. Beta=2.3 is a good compromise for the spectrum of visible light."""
-        if relative_radius >= 1:
-            return 1 / (1 + beta)
-        return (1 + beta * math.sqrt(1 - relative_radius ** 2)) / (1 + beta)
 
 
 class CurveSimBodies(list):
