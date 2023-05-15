a4f7ee1982d70a82156b8a9b8ab819fde8470011
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-05-15 23:48:16 +0200

    cs_bodies.py

--- cs_main.py
+++ cs_main.py
@@ -2,7 +2,7 @@
 # SSLS - Star System Lightcurve Simulator
 # The SSLS calculates the movements and eclipses of celestial bodies and produces a video of this.
 # Specify mass, radius and other properties of some stars and planets in a configuration file.
-# Then run "cs_main.py <Configfilename>" to produce the video.
+# Then run "cs_main.py <configfilename>" to produce the video.
 # The video shows simultanously a view of the star system from the top and from the side and
 # the lightcurve of the system's total luminosity over time.
 # Usually you do not need to look at or even modify the python code. Instead control the program's
@@ -19,28 +19,14 @@ from cs_animation import CurveSimAnimation
 from cs_bodies import CurveSimBodies
 from cs_parameters import CurveSimParameters, Standard_sections
 
-# import configparser
-# import math
-# import matplotlib
-# import matplotlib.animation
-# import numpy as np
-# import time
-#
-# from cs_animation import CurveSimAnimation
-# from cs_body import CurveSimBody
-# from cs_parameters import CurveSimParameters, Standard_sections
-# from cs_physics import CurveSimPhysics
-
-
 # If you run this script in a jupyter notebook, uncomment this line in order to provide the name of your config file.
 # sys.argv[1]="ssls.ini"
 
-
 if __name__ == '__main__':
-    Config = configparser.ConfigParser(inline_comment_prefixes='#')
-    Configfilename = CurveSimParameters.find_and_check_config_file(default="ssls.ini")
-    Config.read(Configfilename)
-    P = CurveSimParameters(Config, Standard_sections)  # Read program parameters from config file.
-    Bodies = CurveSimBodies(P, Configfilename, Standard_sections)  # Read the properties of the physical bodies from the config file and write them into <Bodies>, a list of all physical objects of the simulation.
-    Lightcurve = Bodies.calc_physics(P)  # Calculate body positions and the resulting lightcurve.
-    animation = CurveSimAnimation(P, Bodies, Lightcurve)
+    config = configparser.ConfigParser(inline_comment_prefixes='#')
+    configfilename = CurveSimParameters.find_and_check_config_file(default="ssls.ini")
+    config.read(configfilename)
+    p = CurveSimParameters(config, Standard_sections)  # Read program parameters from config file.
+    bodies = CurveSimBodies(p, configfilename, Standard_sections)  # Initialize the physical bodies, calculate their state vectors and generate their patches for the animation
+    lightcurve = bodies.calc_physics(p)  # Calculate body positions and the resulting lightcurve.
+    animation = CurveSimAnimation(p, bodies, lightcurve)  # Create the video
