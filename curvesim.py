55459ddee92bef1203459288e2bf362e6d71c76d
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-05-17 18:48:05 +0200

    .

--- curvesim.py
+++ curvesim.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-# SSLS - Star System Lightcurve Simulator
-# The SSLS calculates the movements and eclipses of celestial bodies and produces a video of this.
+# Curvesim - A Star System Lightcurve Simulator
+# Curvesim calculates the movements and eclipses of celestial bodies and produces a video of this.
 # Specify mass, radius and other properties of some stars and planets in a configuration file.
 # Then run "curvesim.py <configfilename>" to produce the video.
 # The video shows simultanously a view of the star system from the top and from the side and
 # the lightcurve of the system's total luminosity over time.
 # Usually you do not need to look at or even modify the python code. Instead control the program's
 # outcome with the config file. The meaning of all program parameters is documented in the config file.
-# SSLS uses ffmpeg to convert the data into a video. Download ffmpeg from https://www.ffmpeg.org/download.html.
-# Extract the zip file and add "<yourdriveandpath>\FFmpeg\bin" to Environment Variable PATH.<br>
+# Curvesim uses ffmpeg to convert the data into a video. Download ffmpeg from https://www.ffmpeg.org/download.html.
+# Extract the zip file and add "<yourdriveandpath>\FFmpeg\bin" to Environment Variable PATH.
 #
 # Your questions and comments are welcome.
-# Just open an issue on https://github.com/lichtgestalter/ssls/issues to get my attention :)
+# Just open an issue on https://github.com/lichtgestalter/curvesim/issues to get my attention :)
 
 from cs_animation import CurveSimAnimation
 from cs_bodies import CurveSimBodies
@@ -31,6 +31,6 @@ def curvesim():
 
 if __name__ == '__main__':
     parameters_, bodies_, lightcurve_ = curvesim()
-    print(parameters_)
+    # print(parameters_)
     print(bodies_)
     CurveSimBodies.prettyprint_lightcurve(lightcurve_)
