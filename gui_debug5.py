7a4b9bf3b6f11c60240e9aa000f115e5b47e46f8
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2024-05-11 23:22:27 +0200

    .

--- gui_debug5.py
+++ gui_debug5.py
@@ -9,21 +9,41 @@ class PointViewer(tk.Canvas):
         self.axes_length = axes_length
         self.position_delta = position_delta
         self.point_radius = point_radius
-        self.canvas_center = [self.canvas_size[0] // 2, self.canvas_size[1] // 2]
-        self.offset = self.canvas_center.copy()   # Used to keep the origin (0, 0, 0) in the middle of the canvas
+        self.canvas_center = [self.canvas_size[0] // 2 + 20, self.canvas_size[1] // 2 - 20]
+        # self.offset = self.canvas_center.copy()   # Used to keep the origin (0, 0, 0) in the middle of the canvas
+        self.offset = [0, 0]
+        print(f"Offini: {self.offset[0]:.0f}, {self.offset[1]:.0f}")
+
         self.pack()
         self.create_buttons()
         self.update_canvas()
 
+    def update_canvas(self):
+        self.delete("all")
+        self.update_offset()
+        self.draw_axes()
+        self.draw_points()
+        self.display_viewer_position()
+
+    def update_offset(self):
+        self.origin = self.project_point([0, 0, 0])
+        # self.offset = self.project_point([0, 0, 0])
+        print(f"Origin: {self.origin[0]:.0f}, {self.origin[1]:.0f}")
+        print(f"Offset: {self.offset[0]:.0f}, {self.offset[1]:.0f}")
+        # self.offset[0] -= self.canvas_center[0]
+        # self.offset[1] -= self.canvas_center[1]
+        self.offset[0] = self.origin[0] - self.canvas_center[0]
+        self.offset[1] = self.origin[1] - self.canvas_center[1]
+
     def draw_axes(self):
         pad = 10
-        origin = self.project_point([0, 0, 0])
+        # origin = self.project_point([0, 0, 0])
         x_arrow = self.project_point([self.axes_length, 0, 0])
         y_arrow = self.project_point([0, self.axes_length, 0])
         z_arrow = self.project_point([0, 0, self.axes_length])
-        self.create_line(origin[0], origin[1], x_arrow[0], x_arrow[1], fill="red")
-        self.create_line(origin[0], origin[1], y_arrow[0], y_arrow[1], fill="green")
-        self.create_line(origin[0], origin[1], z_arrow[0], z_arrow[1], fill="blue")
+        self.create_line(self.origin[0], self.origin[1], x_arrow[0], x_arrow[1], fill="red")
+        self.create_line(self.origin[0], self.origin[1], y_arrow[0], y_arrow[1], fill="green")
+        self.create_line(self.origin[0], self.origin[1], z_arrow[0], z_arrow[1], fill="blue")
         self.create_text(x_arrow[0], x_arrow[1] + pad, text="x", fill="red")
         self.create_text(y_arrow[0], y_arrow[1] + pad, text="y", fill="green")
         self.create_text(z_arrow[0], z_arrow[1] + pad, text="z", fill="blue")
@@ -62,25 +82,6 @@ class PointViewer(tk.Canvas):
         position_text = f"Pos: {self.viewer_position[0]}, {self.viewer_position[1]}, {self.viewer_position[2]} Offset: {self.offset[0]:.0f}, {self.offset[1]:.0f}"
         self.create_text(10, 10, text=position_text, anchor="nw")
 
-    def create_buttons(self):
-        button_texts = ["x-", "x+", "y-", "y+", "z-", "z+"]
-        button_commands = [self.move_x_neg, self.move_x_pos, self.move_y_neg, self.move_y_pos, self.move_z_neg, self.move_z_pos]
-        for command, text in zip(button_commands, button_texts):
-            button = tk.Button(self.master, text=text, command=command)
-            button.pack(side="left")
-
-    def update_offset(self):
-        self.offset = self.project_point([0, 0, 0])
-        self.offset[0] -= self.canvas_center[0]
-        self.offset[1] -= self.canvas_center[1]
-
-    def update_canvas(self):
-        self.delete("all")
-        self.update_offset()
-        self.draw_axes()
-        self.draw_points()
-        self.display_viewer_position()
-
     def move_x_neg(self):
         self.viewer_position[0] -= self.position_delta
         self.update_canvas()
@@ -105,9 +106,16 @@ class PointViewer(tk.Canvas):
         self.viewer_position[2] += self.position_delta
         self.update_canvas()
 
+    def create_buttons(self):
+        button_texts = ["x-", "x+", "y-", "y+", "z-", "z+"]
+        button_commands = [self.move_x_neg, self.move_x_pos, self.move_y_neg, self.move_y_pos, self.move_z_neg, self.move_z_pos]
+        for command, text in zip(button_commands, button_texts):
+            button = tk.Button(self.master, text=text, command=command)
+            button.pack(side="left")
+
 
 def main():
-    root = tk.Tk()
+    main_window = tk.Tk()
     points = [
         (30.00, 0.00, 0.00),
         (29.10, 6.76, 6.76),
@@ -122,12 +130,12 @@ def main():
         (28.33, -9.20, -9.20),
     ]
     viewer_start_position = [100, 100, 1000]
-    canvas_size = (600, 600)
+    canvas_size = (1300, 900)
     axes_length = 200
     position_delta = 100
     point_radius = 2
-    app = PointViewer(root, points, viewer_start_position, canvas_size, axes_length, position_delta, point_radius)
-    root.mainloop()
+    PointViewer(main_window, points, viewer_start_position, canvas_size, axes_length, position_delta, point_radius)
+    main_window.mainloop()
 
 
 if __name__ == "__main__":
