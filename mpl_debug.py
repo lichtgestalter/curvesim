7af78486661b9a2ebe41c4f642090a7a879f6adb
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2024-05-16 11:52:15 +0200

    .

--- mpl_debug.py
+++ mpl_debug.py
@@ -8,30 +8,35 @@ def set_axs(axs, ax_index, focal_length, testy):
     print(axs[ax_index].get_proj())
     print(testy)
 
+def main():
+    fig, axs = plt.subplots(1, 3, subplot_kw={'projection': '3d'})
 
-fig, axs = plt.subplots(1, 3, subplot_kw={'projection': '3d'})
+    # Get the test data
+    x, y, z = axes3d.get_test_data(0.09)
 
-# Get the test data
-X, Y, Z = axes3d.get_test_data(0.09)
-points = [
-    (30.00, 0.00, 0.00),
-    (29.10, 6.76, 6.76),
-    (23.11, 18.42, 18.42),
-    (- 3.53, 37.73, 37.73),
-    (- 106.35, 47.04, 47.04),
-    (- 149.96, -30.32, -30.32),
-    (- 142.39, -34.84, -34.84),
-    (1.57, -35.27, -35.27),
-    (9.43, -30.68, -30.68),
-    (25.51, -14.96, -14.96),
-    (28.33, -9.20, -9.20),
-]
+    points = [
+        (30.00, 0.00, 0.00),
+        (29.10, 6.76, 6.76),
+        (23.11, 18.42, 18.42),
+        (- 3.53, 37.73, 37.73),
+        (- 106.35, 47.04, 47.04),
+        (- 149.96, -30.32, -30.32),
+        (- 142.39, -34.84, -34.84),
+        (1.57, -35.27, -35.27),
+        (9.43, -30.68, -30.68),
+        (25.51, -14.96, -14.96),
+        (28.33, -9.20, -9.20),
+    ]
 
-# Plot the data
-for ax in axs:
-    ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+    # Plot the data
+    for ax in axs:
+        ax.plot_wireframe(x, y, z, rstride=10, cstride=10)
 
-for ax_index in range(3):
-    set_axs(axs, ax_index, 10, 42 + ax_index * 5)
+    for ax_index in range(3):
+        set_axs(axs, ax_index, 10, 42 + ax_index * 5)
 
-plt.show()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
