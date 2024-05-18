13646b9bb84b76b2467e5e2ce0a304290fe5f4b8
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2024-05-18 22:51:46 +0200

    .

--- mpl_debug.py
+++ mpl_debug.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 def set_axs(axs, ax_index, focal_length):
     axs[ax_index].set_proj_type('persp', focal_length=focal_length)
@@ -8,6 +8,26 @@ def set_axs(axs, ax_index, focal_length):
     print(ax_index)
     print(axs[ax_index].get_position())
     print(axs[ax_index].get_proj())
+
+
+def plot_planes(axs):
+    minx, miny, minz = -200, -200, -200
+    maxx, maxy, maxz = 200, 200, 200
+    colorx, colory, colorz = 'red', 'blue', 'green'
+    alpha = 0.3
+    # Plot a blue transparent (alpha=0.3) rectangle which represents the y=0 plane. The 3D coordinate
+    x0_plane = [(0, miny, minz), (0, miny, maxz), (0, maxy, maxz), (0, maxy, minz)]
+    y0_plane = [(minx, 0, minz), (minx, 0, maxz), (maxx, 0, maxz), (maxx, 0, minz)]
+    z0_plane = [(minx, miny, 0), (minx, maxy, 0), (maxx, maxy, 0), (maxx, miny, 0)]
+    for ax in axs:
+        # show the y=0 plane as a blue, transparent polygon in the x-z plane
+        poly_x = Poly3DCollection([x0_plane], color=colorx, alpha=alpha)
+        poly_y = Poly3DCollection([y0_plane], color=colory, alpha=alpha)
+        poly_z = Poly3DCollection([z0_plane], color=colorz, alpha=alpha)
+        ax.add_collection3d(poly_x)
+        ax.add_collection3d(poly_y)
+        ax.add_collection3d(poly_z)
+
 
 def main():
     fig, axs = plt.subplots(1, 3, subplot_kw={'projection': '3d'})
@@ -32,8 +52,9 @@ def main():
         ax.plot(x, y, z, color='black')  # Add a black line connecting the points
         ax.scatter(x, y, z)
 
+    plot_planes(axs)
+
     for ax_index in range(3):
-        # axs[ax_index].set_proj_type('persp', focal_length=10)
         set_axs(axs, ax_index, 10)
 
     plt.show()
@@ -41,37 +62,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# --------------------
-# To show the y=0 plane and z=0 plane transparently in each plot, you can
-# use the axvline and axhline functions in Matplotlib. You can set the
-# transparency level using the alpha parameter. Here is an example of how
-# you can add transparent planes to your plots:
-# In this code snippet, the axhline function is used to plot the y=0 plane,
-# and the axvline function is used to plot the z=0 plane. The alpha parameter
-# in these functions controls the transparency level of the planes. You can
-# adjust the alpha value to make the planes more or less transparent.
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
-#
-# def set_axs(axs, ax_index, focal_length):
-#     axs[ax_index].set_proj_type('persp', focal_length=focal_length)
-#     axs[ax_index].set_title(f" testtitle {ax_index}", fontsize=10)
-#
-#     # Plot transparent y=0 plane
-#     axs[ax_index].axhline(0, color='blue', alpha=0.3)
-#
-#     # Plot transparent z=0 plane
-#     axs[ax_index].axvline(0, color='green', alpha=0.3)
-#
-#     print(ax_index)
-#     print(axs[ax_index].get_position())
-#     print(axs[ax_index].get_proj())
-#
-# def main():
-#     fig, axs = plt.subplots(1, 3, subplot_kw={'projection': '3d'})
-#
-#     for i in range(3):
-#         set_axs(axs, i, focal_length=1.0)
-#
-#     plt.show()
