5d56eab4fd8895d92ecd234aa053df1b65ebb05a
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2024-05-19 20:55:11 +0200

    .

--- mpl_debug.py
+++ mpl_debug.py
@@ -1,36 +1,33 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-def set_axs(axs, ax_index, focal_length):
-    axs[ax_index].set_proj_type('persp', focal_length=focal_length)
-    axs[ax_index].set_title(f" testtitle {ax_index}", fontsize=10)
-    axs[ax_index].view_init(elev=10*ax_index, azim=90+30*ax_index)
-    print(ax_index)
-    print(axs[ax_index].get_position())
-    print(axs[ax_index].get_proj())
+def set_axs(axs, elev, azim,focal_length):
+    axs.set_proj_type('persp', focal_length=focal_length)
+    axs.set_title(f" testtitle", fontsize=10)
+    axs.view_init(elev=elev, azim=azim)
+    print(axs.get_position())
+    print(axs.get_proj())
 
 
-def plot_planes(axs):
-    minx, miny, minz = -200, -200, -200
-    maxx, maxy, maxz = 200, 200, 200
+def plot_planes(axs, alpha=0.3, min_=-200, max_=200):
+    # show the x=0, y=0 and z=0 plane as transparent polygons
+    minx, miny, minz = min_, min_, min_
+    maxx, maxy, maxz = max_, max_, max_
     colorx, colory, colorz = 'red', 'blue', 'green'
-    alpha = 0.3
-    # Plot a blue transparent (alpha=0.3) rectangle which represents the y=0 plane. The 3D coordinate
     x0_plane = [(0, miny, minz), (0, miny, maxz), (0, maxy, maxz), (0, maxy, minz)]
     y0_plane = [(minx, 0, minz), (minx, 0, maxz), (maxx, 0, maxz), (maxx, 0, minz)]
     z0_plane = [(minx, miny, 0), (minx, maxy, 0), (maxx, maxy, 0), (maxx, miny, 0)]
-    for ax in axs:
-        # show the y=0 plane as a blue, transparent polygon in the x-z plane
-        poly_x = Poly3DCollection([x0_plane], color=colorx, alpha=alpha)
-        poly_y = Poly3DCollection([y0_plane], color=colory, alpha=alpha)
-        poly_z = Poly3DCollection([z0_plane], color=colorz, alpha=alpha)
-        ax.add_collection3d(poly_x)
-        ax.add_collection3d(poly_y)
-        ax.add_collection3d(poly_z)
+    poly_x = Poly3DCollection([x0_plane], color=colorx, alpha=alpha)
+    poly_y = Poly3DCollection([y0_plane], color=colory, alpha=alpha)
+    poly_z = Poly3DCollection([z0_plane], color=colorz, alpha=alpha)
+    axs.add_collection3d(poly_x)
+    axs.add_collection3d(poly_y)
+    axs.add_collection3d(poly_z)
 
 
 def main():
-    fig, axs = plt.subplots(1, 3, subplot_kw={'projection': '3d'})
+    fig, axs = plt.subplots(1, 1, subplot_kw={'projection': '3d'})
+    set_axs(axs, 30, 120, 10)  # define projection and view
 
     points = [
         (131.00, 0.00, 0.00),
@@ -47,15 +44,10 @@ def main():
     ]
     x, y, z = zip(*points)
 
-    # Plot the data
-    for ax in axs:
-        ax.plot(x, y, z, color='black')  # Add a black line connecting the points
-        ax.scatter(x, y, z)
+    axs.scatter(x, y, z)  # Plot the points
+    axs.plot(x, y, z, color='black')  # connect the points
+    plot_planes(axs)  # show the x=0, y=0 and z=0 plane as transparent polygons
 
-    plot_planes(axs)
-
-    for ax_index in range(3):
-        set_axs(axs, ax_index, 10)
 
     plt.show()
 
